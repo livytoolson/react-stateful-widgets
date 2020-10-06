38875ab9f6905e118e5f8f80e196c3653d9cc6d7
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-05 19:42:52 -0600

    attempted programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -30,6 +30,7 @@ export default function Programmers() {
   // The programmers list on the one hand, and the id of the featured programmer on the other.
   const [programmer, setProgrammer] = useState(listOfAwesome)
   const [featured, setFeatured] = useState()
+  // console.log(programmer)
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -37,7 +38,9 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return 
+    programmer.filter(dev => {
+      return (programmer == featured) ? dev.name : ' ';
+    })
   };
 
   const style = {
